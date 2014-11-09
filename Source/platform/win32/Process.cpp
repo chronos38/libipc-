dc1e2e6348b24ce505e32f2ad6349b698c6a2cd9
David Wolf <d.wolf@live.at>
2014-11-09 15:38:27 +0100

    usings

--- Source/platform/win32/Process.cpp
+++ Source/platform/win32/Process.cpp
@@ -1,21 +1,34 @@
 #ifdef _MSC_VER
 #include "../../../include/libipcpp/Process.h"
 
-using namespace std;
+using string = std::string;
+template <typename T>
+using vector = std::vector < T > ;
 
 namespace ipc {
     Process::Process(const string& fileName, const vector<string>& args)
     {
     }
+
     Process::Process(const string& fileName, const string& args)
     {
-        LPSTR cmdl = new CHAR[args.size()];
+        LPSTR cmdl = new CHAR[args.size() + 1];
         STARTUPINFO startupInfo;
         PROCESS_INFORMATION processInformation;
         memset(&startupInfo, 0, sizeof(startupInfo));
+        memset(cmdl, 0, args.size() + 1);
         memcpy(cmdl, args.data(), args.size());
 
-        BOOL result = CreateProcessA(fileName.c_str(), args, NULL, NULL, FALSE, 0, NULL, NULL, &startupInfo, &processInformation);
+        BOOL result = CreateProcessA(fileName.c_str(), cmdl, NULL, NULL, FALSE, 0, NULL, NULL, &startupInfo, &processInformation);
+        delete cmdl;
+
+        if (!result) {
+            // TODO: Systeminformation abrufen und als Argument übergeben.
+            throw ProcessException("");
+        } else {
+            mIsOwner = true;
+            mProcess = processInformation.hProcess;
+        }
     }
 
     Process::Process(Process&& p)
