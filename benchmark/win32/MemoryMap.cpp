2e680e47972b328b75ebc2e2db28ed181a91a8b2
David Wolf <d.wolf@live.at>
2015-01-11 18:58:25 +0100

    Benchmark Native Win32

--- benchmark/win32/MemoryMap.cpp
+++ benchmark/win32/MemoryMap.cpp
@@ -2,5 +2,38 @@
 
 std::chrono::duration<double, std::ratio<1, 1000>> CallNativeMemoryMap(unsigned dataSize, unsigned iterations, CallMethod method)
 {
-    return std::chrono::duration<double>(0);
+    auto handle = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE | SEC_COMMIT, 0, dataSize * iterations, NULL);
+    auto sharedBuffer = MapViewOfFile(handle, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+    char* buffer = new char[dataSize];
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+
+    switch (method) {
+    case Read:
+        start = std::chrono::high_resolution_clock::now();
+        for (unsigned i = 0; i < iterations; i++) {
+            CopyMemory(buffer, (char*)sharedBuffer, dataSize);
+        }
+        end = std::chrono::high_resolution_clock::now();
+        break;
+    case Write:
+        start = std::chrono::high_resolution_clock::now();
+        for (unsigned i = 0; i < iterations; i++) {
+            CopyMemory((char*)sharedBuffer, buffer, dataSize);
+        }
+        end = std::chrono::high_resolution_clock::now();
+        break;
+    case ReadWrite:
+        start = std::chrono::high_resolution_clock::now();
+        for (unsigned i = 0; i < iterations; i++) {
+            CopyMemory((char*)sharedBuffer, buffer, dataSize);
+            CopyMemory(buffer, (char*)sharedBuffer, dataSize);
+        }
+        end = std::chrono::high_resolution_clock::now();
+        break;
+    }
+
+    delete buffer;
+    UnmapViewOfFile(sharedBuffer);
+    CloseHandle(handle);
+    return std::chrono::duration_cast<std::chrono::duration<double, std::ratio<1, 1000>>>(end - start);
 }
--- benchmark/win32/Pipe.cpp
+++ benchmark/win32/Pipe.cpp
@@ -2,5 +2,37 @@
 
 std::chrono::duration<double, std::ratio<1, 1000>> CallNativePipe(unsigned dataSize, unsigned iterations, CallMethod method)
 {
-    return std::chrono::duration<double>(0);
+    HANDLE write, read;
+    CreatePipe(&read, &write, NULL, 65536);
+    char* buffer = new char[dataSize];
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    DWORD n = 0;
+
+    switch (method) {
+    case Read:
+        start = std::chrono::high_resolution_clock::now();
+        for (unsigned i = 0; i < iterations; i++) {
+            ReadFile(read, buffer, dataSize, &n, NULL);
+        }
+        end = std::chrono::high_resolution_clock::now();
+        break;
+    case Write:
+        start = std::chrono::high_resolution_clock::now();
+        for (unsigned i = 0; i < iterations; i++) {
+            WriteFile(write, buffer, dataSize, &n, NULL);
+        }
+        end = std::chrono::high_resolution_clock::now();
+        break;
+    case ReadWrite:
+        start = std::chrono::high_resolution_clock::now();
+        for (unsigned i = 0; i < iterations; i++) {
+            WriteFile(write, buffer, dataSize, &n, NULL);
+            ReadFile(read, buffer, dataSize, &n, NULL);
+        }
+        end = std::chrono::high_resolution_clock::now();
+        break;
+    }
+
+    delete buffer;
+    return std::chrono::duration_cast<std::chrono::duration<double, std::ratio<1, 1000>>>(end - start);
 }
